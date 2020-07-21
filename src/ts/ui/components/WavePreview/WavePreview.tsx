0e9ca226f7cc8d8031a7fc0e8075df2068789690
FlopieUtd <florisdehaan92@hotmail.com>
2020-07-21 23:20:23 +0200

    feat: Update preview styling

--- src/ts/ui/components/WavePreview/WavePreview.tsx
+++ src/ts/ui/components/WavePreview/WavePreview.tsx
@@ -15,21 +15,21 @@ export const WavePreview = observer(() => {
           const color = enemyBlueprints.find(enemy => enemy.type === wave.type).color;
           return <WaveLine {...wave} color={color} key={index} />;
         })}
-        <div className="wave-preview-dropdown">
-          {waves.slice(currentWaveGroup + 1).map((waveGroup, i) => (
-            <div className="wave-preview" key={i}>
-              {waveGroup.map((wave, j) => {
-                const typeColor =
-                  enemyBlueprints.find(enemy => enemy.type === wave.type).color ?? "white";
-                return (
-                  <div className="wave-line" key={j} style={{ color: typeColor }}>
-                    {wave.amount} x {wave.type}
-                  </div>
-                );
-              })}
-            </div>
-          ))}
-        </div>
+      </div>
+      <div className="wave-preview-dropdown">
+        {waves.slice(currentWaveGroup + 1).map((waveGroup, i) => (
+          <div className="wave-preview" key={i}>
+            {waveGroup.map((wave, j) => {
+              const typeColor =
+                enemyBlueprints.find(enemy => enemy.type === wave.type).color ?? "white";
+              return (
+                <div className="wave-line" key={j} style={{ color: typeColor }}>
+                  {wave.amount} x {wave.type}
+                </div>
+              );
+            })}
+          </div>
+        ))}
       </div>
     </div>
   );
